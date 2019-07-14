f86eff691d71bf5a6a82df99c0b9b4846562d5ae
Asshley <ashwafaw@gmail.com>
2019-07-13 21:09:15 -0400

    ref/fix: should err if no gov roles, use array for easier maintenance

--- src/commands/general/resign.js
+++ src/commands/general/resign.js
@@ -16,11 +16,12 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 'use strict';
-const { Command } = require('patron.js');
+const { Command, CommandResult } = require('patron.js');
 const client = require('../../services/client.js');
 const db = require('../../services/database.js');
 const catch_discord = require('../../utilities/catch_discord.js');
 const discord = require('../../utilities/discord.js');
+const system = require('../../utilities/system.js');
 const edit_member = catch_discord(client.editGuildMember.bind(client));
 
 module.exports = new class Resign extends Command {
@@ -33,23 +34,20 @@ module.exports = new class Resign extends Command {
   }
 
   async run(msg) {
-    const {
-      officer_role, judge_role, congress_role,
-      chief_justice_role, chief_officer_role, house_speaker_role
-    } = db.fetch('guilds', { guild_id: msg.channel.guild.id });
-    const roles = [
-      officer_role,
-      judge_role,
-      congress_role,
-      chief_justice_role,
-      chief_officer_role,
-      house_speaker_role
-    ].filter(x => msg.channel.guild.roles.has(x));
+    const res = db.fetch('guilds', { guild_id: msg.channel.guild.id });
+    const roles = system.gov_roles.concat(system.chief_roles).filter(
+      x => res[x] && msg.channel.guild.roles.has(res[x]) && msg.member.roles.includes(res[x])
+    );
+
+    if (!roles.length) {
+      return CommandResult.fromError('You don\'t have any government official roles.');
+    }
+
     const copy = msg.member.roles.slice();
 
     for (let i = 0; i < roles.length; i++) {
       const role = roles[i];
-      const index = msg.member.roles.indexOf(role);
+      const index = msg.member.roles.indexOf(res[role]);
 
       if (index !== -1) {
         copy.splice(index, 1);
