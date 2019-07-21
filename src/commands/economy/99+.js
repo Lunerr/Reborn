e45e92069c05b4a02391c4148280bb4a5e3cdd0e
Asshley <ashwafaw@gmail.com>
2019-07-21 15:56:18 -0400

    fix: payout should be x - 1

--- src/commands/economy/99+.js
+++ src/commands/economy/99+.js
@@ -1,6 +1,6 @@
 const Gambling = require('../../templates/gambling.js');
 const odds = 99;
-const payout = 90;
+const payout = 89;
 
 module.exports = new Gambling(
   ['99+'], 'Roll 99.00 or higher on a 100.00 sided die to win 90X your bet.', odds, payout
